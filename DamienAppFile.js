4e062c85fac6e7b97e3519e25d307ceb9b3ddd8e
Frank Siri <franksiri@gmail.com>
2018-09-14 21:09:46 -0400

    tell Damien he is hitting the right chords

---
+++ DamienAppFile.js
@@ -0,0 +1,13 @@
+var handler = function (request, response) {
+  console.log('Received request from ' + request.connection.remoteAddress)
+  response.writeHead(200)
+  response.end("Nice work! You succeeded in hitting... " + os.hostname()
+    + ', on ' + new Date + '\n')
+}
+
+var www = http.createServer(handler)
+
+var portNumber = 8080
+www.listen(portNumber)
+
+console.log('My web server listening on ...' + portNumber)
---
+++ README.md
@@ -0,0 +1,2 @@
+# DamienTest
+# DamienTest
