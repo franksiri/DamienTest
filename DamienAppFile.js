7738cc864b87097ba444d4745cfa73b2f73a1505
Frank Siri <franksiri@gmail.com>
2018-09-16 13:04:45 -0400

    second commit

--- DamienAppFile.js
+++ DamienAppFile.js
@@ -1,3 +1,9 @@
+const http = require('http')
+const os = require('os')
+
+console.log('My original web server starting...')
+
+
 var handler = function (request, response) {
   console.log('Received request from ' + request.connection.remoteAddress)
   response.writeHead(200)
